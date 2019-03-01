06141925d9f160e2c7312bff1f3b9660ea242f1f
Rofed <ron99f@gmail.com>
2019-03-01 22:12:17 +0200

    adding xml example configuration for spring Bean

--- src/main/java/com/ronfederman/chucknorrisjokes/ChuckNorrisJokesApplication.java
+++ src/main/java/com/ronfederman/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -3,8 +3,10 @@ package com.ronfederman.chucknorrisjokes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokesApplication {
 
     public static void main(String[] args) {
